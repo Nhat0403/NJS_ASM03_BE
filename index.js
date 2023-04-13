89503bf4f1c8cdc8c2e445fbe2893e26cb7baa64
Nhat0403 <makarongktk7@gmail.com>
2023-04-13 13:23:46 +0700

    add cors allow headers

--- index.js
+++ index.js
@@ -50,8 +50,9 @@ const corsOptions = {
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Access-Control-Allow-Headers, Origin, Accept');
   next();
 });
 
